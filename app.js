f2b4d5a1a73ed2896da5cd344cbbd2cabd143f0f
Ericcentrik <enaiman1@gmail.com>
2019-04-27 22:13:24 -0400

    created db, created event data model, connected to remote db for test

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// dependecies
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+// connect to event DB
+const Event = require('./models/event');
+
+// connecting express
 const app = express();
-
 
 
 app.use(bodyParser.json());
@@ -41,23 +46,48 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;     
+           return Event.find()
+            .then(events => {
+                return events.map(event => {
+                    return { ...event._doc, _id: event.id };
+                });
+            })
+            .catch(err => {
+                throw err;
+            })
         },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+        createEvent: args => {
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event);
-            return event;
+                date: new Date(args.eventInput.date)
+            })
+            // Lets graphql know this is an async event
+            // the .save is a mongoose method to write and save info into DB
+           return event.save().then( result => {
+               console.log(result);
+            // propety that give us all the core properties of out object/leave out meta data
+               return { ...result._doc, _id: event.id  };
+           }).catch(err => {
+               console.log(err);
+               throw err;
+           });
         }
     },
     graphiql: true
 })
 );
+
+// connecting db
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+    }@cluster0-hh00e.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+).then().catch(err => {
+    console.log(err);
+});
 
 app.listen(3000);
 
